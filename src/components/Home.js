92fdf93a47fa9744736d9152fe276ce7d11cbd7f
Brandon Lingo <brandonlingo0@gmail.com>
2023-12-31 02:31:43 -0800

    created basic home page

---
+++ src/components/Home.js
@@ -0,0 +1,49 @@
+const Home = () => {
+    return (
+        <>
+            <div className="title">
+                <h1>Brandon Lingo</h1>
+                <p>Software Devolper</p>
+            </div>
+
+            <div className="main-container">
+
+                <div className="python-logo">
+                    <i class="fa-brands fa-python fa-3x"></i>
+                    <p>Python</p>
+                </div>
+
+                <div className="js-logo">
+                <i class="fa-brands fa-js fa-3x"></i>
+                <p>JavaScript</p>
+                </div>
+
+                <div className="html-logo">
+                    <i class="fa-brands fa-html5 fa-3x"></i>
+                    <p>HTML</p>
+                </div>
+
+                <div className="css-logo">
+                    <i class="fa-brands fa-css3-alt fa-3x"></i>
+                    <p>CSS</p>
+                </div>
+
+                <div className="react-logo">
+                    <i class="fa-brands fa-react fa-3x"></i>
+                    <p>React</p>
+                </div>
+                
+                <div className="java-logo">
+                    <i class="fa-brands fa-java fa-3x"></i>
+                    <p>Java</p>
+                </div>
+
+                <div className="center-logo">
+                    <i className="fa-solid fa-laptop-code fa-10x"></i>
+                </div>
+            </div>
+
+        </>);
+}
+
+export default Home;
