a790866d7aa5424fecb21453733f8bfff70de048
Brandon Lingo <brandonlingo0@gmail.com>
2023-12-31 16:49:56 -0800

    add search feature

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import Home from "./components/Home";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Projects from "./components/Projects";
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Home />
+ 
+
+      <Switch>
+
+        <Route exact path="/">
+          <Home></Home>
+        </Route>
+
+        <Route path="/projects">
+          <Projects></Projects>
+        </Route>
+      </Switch>
       </div>
     </Router>
+
   );
 }
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,53 +1,80 @@
-import {Link} from 'react-router-dom'
+import { Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, useHistory } from "react-router-dom/cjs/react-router-dom.min";
+import Projects from "./Projects";
+import {useEffect} from 'react'
 
 const Home = () => {
-    return (
-        <>
-            <div className="title">
-                <h1>Brandon Lingo</h1>
-                <p>Software Devolper</p>
-            </div>
 
-            <div className="main-container">
+    useEffect(() => console.log('hi'))
+    const history = useHistory()
 
-                <div className="python-logo">
-                    <i class="fa-brands fa-python fa-3x"></i>
-                    <p>Python</p>
-                </div>
+    const handleSearch = () => {
 
-                <div className="js-logo">
-                <i class="fa-brands fa-js fa-3x"></i>
-                <p>JavaScript</p>
-                </div>
+        const searchBarValue = document.getElementById("search-input").value.toLowerCase()
+        console.log(searchBarValue)
 
-                <div className="html-logo">
-                    <i class="fa-brands fa-html5 fa-3x"></i>
-                    <p>HTML</p>
-                </div>
+        history.push(`/${searchBarValue}`)
+    }
 
-                <div className="css-logo">
-                    <i class="fa-brands fa-css3-alt fa-3x"></i>
-                    <p>CSS</p>
-                </div>
+  return (
+    <>
+      <div className="title">
+        <h1>Brandon Lingo</h1>
+        <p>Software Devolper</p>
+      </div>
 
-                <div className="react-logo">
-                    <i class="fa-brands fa-react fa-3x"></i>
-                    <p>React</p>
-                </div>
-                
-                <div className="java-logo">
-                    <i class="fa-brands fa-java fa-3x"></i>
-                    <p>Java</p>
-                </div>
+      <div className="main-container">
+        <div className="python-logo">
+          <i class="fa-brands fa-python fa-3x"></i>
+          <p>Python</p>
+        </div>
 
-                <Link to="/projects">
-                    <div className="center-logo">
-                        <i className="fa-solid fa-laptop-code fa-10x"></i>
-                    </div>
-                </Link>
-            </div>
+        <div className="js-logo">
+          <i class="fa-brands fa-js fa-3x"></i>
+          <p>JavaScript</p>
+        </div>
 
-        </>);
-}
+        <div className="html-logo">
+          <i class="fa-brands fa-html5 fa-3x"></i>
+          <p>HTML</p>
+        </div>
+
+        <div className="css-logo">
+          <i class="fa-brands fa-css3-alt fa-3x"></i>
+          <p>CSS</p>
+        </div>
+
+        <div className="react-logo">
+          <i class="fa-brands fa-react fa-3x"></i>
+          <p>React</p>
+        </div>
+
+        <div className="java-logo">
+          <i class="fa-brands fa-java fa-3x"></i>
+          <p>Java</p>
+        </div>
+
+        <Link to="/projects">
+          <div className="center-logo">
+            <i className="fa-solid fa-laptop-code fa-10x"></i>
+          </div>
+        </Link>
+      </div>
+
+      <div className="nav-container">
+        <div className="search-container">
+          <input
+            placeholder="ex. About, Projects, Contact"
+            className="search-input"
+            id="search-input"
+          ></input>
+        </div>
+        <button id="search-button" type="submit" onClick={handleSearch}>
+          <i class="fa-solid fa-magnifying-glass fa-2x"></i>
+        </button>
+      </div>
+    </>
+  );
+};
 
 export default Home;
