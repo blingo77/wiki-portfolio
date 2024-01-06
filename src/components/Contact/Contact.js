22436ccaaef10d2469ea5b9bbff9329becd1b154
Brandon Lingo <brandonlingo0@gmail.com>
2024-01-06 13:14:21 -0800

    deploying site

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -8,41 +8,44 @@ const Contact = () => {
 
     const title = 'Contact'
 
-    return ( 
+    return (
         <>
-        <NavBar title={title}></NavBar>
+            <NavBar title={title}></NavBar>
 
-        <div className="contact-container">
-            <div className="contact-title-container">
-                <h1>Contact Info</h1>
-            </div>
+            <div className="contact-container">
+                <div className="contact-title-container">
+                    <h1>Contact Info</h1>
+                </div>
 
-            <div className="contact-main-container">
-                <div className="phone-container">
-                    <img src={phoneLogo}/>
-                    <h2>Phone</h2>
-                    <h3>702-271-4970</h3>
-                </div>
-                <div className="email-container">
-                    <img src={emailLogo}/>
-                    <h2>Email</h2>
-                    <a href="mailto:brandonlingo0@gmail.com">
-                    <h3>brandonlingo0@gmail.com</h3>
+                <div className="contact-main-container">
+                    <a href="tel:7022714970" className="phone-link">
+                        <div className="phone-container">
+                            <img src={phoneLogo} />
+                            <h2><u>Phone</u></h2>
+                            <h3>702-271-4970</h3>
+                        </div>
                     </a>
-                    <h5>Or</h5>
-                    <a href="mailto:brandonlingo2004@gmail.com">
-                    <h3>brandonlingo2004@gmail.com</h3>
-                    </a>
-                </div>
-                <div className="gitHub-container">
-                    <img src={gitHubLogo}/>
-                    <h2>GitHub</h2>
-                    <h3>Blingo77</h3>
+                    <div className="email-container">
+                        <a href="mailto:brandonlingo0@gmail.com"><img src={emailLogo} /></a>
+                        <u><h2>Email</h2></u>
+                        <a href="mailto:brandonlingo0@gmail.com">
+                            <h3>brandonlingo0@gmail.com</h3>
+                        </a>
+                        <h5>Or</h5>
+                        <a href="mailto:brandonlingo2004@gmail.com">
+                            <h3>brandonlingo2004@gmail.com</h3>
+                        </a>
+                    </div>
+                    <a href="https://github.com/blingo77" className="gitHub-link">
+                        <div className="gitHub-container">
+                            <img src={gitHubLogo} />
+                            <u><h2 >GitHub</h2></u>
+                            <h3>Blingo77</h3>
+                        </div></a>
                 </div>
             </div>
-        </div>
         </>
-     );
+    );
 }
- 
+
 export default Contact;
